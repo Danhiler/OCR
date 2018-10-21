0ec6e6979a5d3a64044fc6d0b44ad463f4ae4c9f
danhiler@gmail.com <danhiler@gmail.com>
2018-10-21 15:29:28 +0300

    added port as for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Tesseract = require('tesseract.js')
 const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 app.use(bodyParser.json())
