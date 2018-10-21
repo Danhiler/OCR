04667eb30bb96aad1bf3c85951f835b2ea7075ee
danhiler@gmail.com <danhiler@gmail.com>
2018-10-21 15:07:49 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+var Tesseract = require('tesseract.js')
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+
+    res.send('Hello World!')
+})
+
+app.post('/getTextFromImage',(req,res)=>{
+    try{
+    const string64 = req.body.data
+    if(!string64)
+    res.status(300).send("Please send img based64 under data")
+    var b64string = req.body.data
+    var buffer = Buffer.from(b64string, 'base64');
+
+    Tesseract.recognize(buffer)
+      .progress(function  (p) { console.log('progress', p)  })
+      .catch(err => res.status(300).send("error occurred with OCR engine"))
+      .then(function (result) {
+        console.log(result.text)
+        res.status(200).json({text:result.text+" made by Dan Hilerowicz"})
+        process.exit(0)
+      })
+    }catch(err){
+        res.status(300).send("Unexpected error occurred")
+    }
+
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
