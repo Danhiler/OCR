5d39b08004087b9ffd9bc00854877876c4f8faa6
danhiler@gmail.com <danhiler@gmail.com>
2018-10-21 16:20:41 +0300

    bug fix

--- server.js
+++ server.js
@@ -7,30 +7,40 @@ const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/getTextFromImage',(req,res)=>{
-    try{
-    const string64 = req.body.data
-    if(!string64)
-    res.status(300).send("Please send img based64 under data")
-    var b64string = req.body.data
-    var buffer = Buffer.from(b64string, 'base64');
+app.post('/getTextFromImage', (req, res) => {
+    try {
+        const string64 = req.body.data
+        if (!string64) {
+            res.status(300).send("Please send img based64 under data")
 
-    Tesseract.recognize(buffer)
-      .progress(function  (p) { console.log('progress', p)  })
-      .catch(err => res.status(300).send("error occurred with OCR engine"))
-      .then(function (result) {
-        console.log(result.text)
-        res.status(200).json({text:result.text+" made by Dan Hilerowicz"})
-        process.exit(0)
-      })
-    }catch(err){
+        } else {
+            var buffer = Buffer.from(string64, 'base64');
+
+            Tesseract.recognize(buffer)
+                .progress(function (p) {
+                    console.log('progress', p)
+                })
+                .catch(err => {
+                    res.status(300).send("error occurred with OCR engine")
+                })
+                .then(function (result) {
+                    console.log(result.text)
+                    res.status(200).json({
+                        text: result.text + " made by Dan Hilerowicz"
+                    })
+                    process.exit(0)
+                })
+        }
+    } catch (err) {
         res.status(300).send("Unexpected error occurred")
     }
 
