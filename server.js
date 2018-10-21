ab6dfaf5bfec3c0977a3e7ccbb52210c20dae715
danhiler@gmail.com <danhiler@gmail.com>
2018-10-21 15:33:41 +0300

    added cors

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ var bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
-
     res.send('Hello World!')
 })
 
